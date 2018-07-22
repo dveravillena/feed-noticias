1ebee6767fe9c24140e0dea8760743c88948d89f
David Vera <dveravillena@gmail.com>
2018-07-22 15:21:53 +0200

    Modificaciones: -Creada la vista de creación de Feed

--- application/views/feed/newfeed.php
+++ application/views/feed/newfeed.php
@@ -1,9 +1,38 @@
-Crear Nuevo Feed
-<form action='save' method='post'>
-  <label>Título:</label><br><input type='text' name='title'><br>
-  <label>Descripción:</label><br><input type='text' name='description'><br>
-  <label>Imagen:</label><br><input type='text' name='image'><br>
-  <label>Fuente:</label><br><input type='text' name='source'><br>
-  <label>Publicado por:</label><br><input type='text' name='publisher'><br>
-	<br><input type='submit' value='Guardar'>
-</form>
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-md-12">
+      <h1 class="text-center">Crear Nuevo Feed</h1>
+      <br><br>
+    </div>
+  </div>
+	<div class="row">
+		<div class="col-md-8 col-md-offset-2">
+			<form action='save' method='post'>
+					<div class="form-group">
+						<label>Título:</label>
+						<input type="text" class="form-control" name='title' placeholder="Título" maxlength="300">
+					</div>
+					<div class="form-group">
+				    <label>Descripción:</label>
+				    <textarea class="form-control" name='description' rows="3"></textarea>
+				  </div>
+					<div class="form-group">
+						<label>Imagen:</label>
+						<input type="text" class="form-control" name='image' placeholder="Imagen" maxlength="300">
+					</div>
+					<div class="form-group">
+						<label>Fuente:</label>
+						<input type="text" class="form-control" name='source' placeholder="Fuente" maxlength="200">
+					</div>
+					<div class="form-group">
+						<label>Publicado por:</label>
+						<input type="text" class="form-control" name='publisher' placeholder="Publisher" maxlength="100">
+					</div>
+					<div class="text-center">
+						<button type="submit" class="btn btn-primary">Guardar</button>
+						<a class="btn btn-danger" href="../" role="button">Cancelar</a>
+					</div>
+			</form>
+		</div>
+	</div>
+</div>
